3b1b4ca9e2032b1cca087c10613a14595cee59fe
Juan Cruz <juanmir227@gmail.com>
2025-12-07 19:12:27 -0300

    arreglo de domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://TU-DOMINIO.com",
+  site: "https://juancmiranda.netlify.app",
   integrations: [sitemap()],
 });
